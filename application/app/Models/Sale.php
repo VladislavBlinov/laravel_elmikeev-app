6bf3a62ab2a00aa4db84cc7f40e3ced1202a5cf0
VladislavBlinov <blnvvldslv@gmail.com>
2025-09-25 09:29:32 +0500

    Add fetch sales

---
+++ application/app/Models/Sale.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Sale extends Model
+{
+    protected $fillable = [
+        'g_number',
+        'date',
+        'last_change_date',
+        'supplier_article',
+        'tech_size',
+        'barcode',
+        'total_price',
+        'discount_percent',
+        'is_supply',
+        'is_realization',
+        'promo_code_discount',
+        'warehouse_name',
+        'country_name',
+        'oblast_okrug_name',
+        'region_name',
+        'income_id',
+        'sale_id',
+        'odid',
+        'spp',
+        'for_pay',
+        'finished_price',
+        'price_with_disc',
+        'nm_id',
+        'subject',
+        'category',
+        'brand',
+        'is_storno'
+    ];
+
+    protected $casts = [
+        'date' => 'date',
+        'last_change_date' => 'datetime',
+        'barcode' => 'integer',
+        'total_price' => 'decimal:2',
+        'discount_percent' => 'decimal:2',
+        'is_supply' => 'boolean',
+        'is_realization' => 'boolean',
+        'promo_code_discount' => 'decimal:2',
+        'income_id' => 'integer',
+        'spp' => 'decimal:2',
+        'for_pay' => 'decimal:2',
+        'finished_price' => 'decimal:2',
+        'price_with_disc' => 'decimal:2',
+        'nm_id' => 'integer',
+        'is_storno' => 'boolean',
+    ];
+}
---
+++ application/config/api.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'base_url' => env('API_BASE_URL'),
+    'api_key' => env('API_KEY'),
+];
